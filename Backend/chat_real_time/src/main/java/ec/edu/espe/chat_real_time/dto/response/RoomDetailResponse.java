eddaedbd5ff9c15ece735937335f73d0c234259b
JairoBonilla2004 <2c.bonilla.jairo0@gmail.com>
2025-11-07 11:38:22 -0500

    feat(dto): Define RoomDetailResponse

---
+++ Backend/chat_real_time/src/main/java/ec/edu/espe/chat_real_time/dto/response/RoomDetailResponse.java
@@ -0,0 +1,18 @@
+package ec.edu.espe.chat_real_time.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomDetailResponse {
+  private RoomResponse room;
+  private List<SessionResponse> activeSessions;
+  private List<MessageResponse> recentMessages;
+  private Integer activeUsersCount;
+}
