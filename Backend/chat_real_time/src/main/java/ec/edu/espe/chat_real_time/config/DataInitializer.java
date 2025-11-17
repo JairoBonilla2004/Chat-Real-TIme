68586f89a9f59f30919090fa4c62bf6d19060240
MrBowis <02alejo20@gmail.com>
2025-11-17 00:03:40 -0500

    feat: Add docker config

---
+++ Backend/chat_real_time/src/main/java/ec/edu/espe/chat_real_time/config/DataInitializer.java
@@ -0,0 +1,31 @@
+package ec.edu.espe.chat_real_time.config;
+import ec.edu.espe.chat_real_time.model.Role;
+import ec.edu.espe.chat_real_time.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner init(RoleRepository roleRepository) {
+        return args -> {
+
+            if (roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+                roleRepository.save(Role.builder()
+                        .name("ROLE_ADMIN")
+                        .description("Administrador del sistema")
+                        .build());
+            }
+
+            if (roleRepository.findByName("ROLE_GUEST").isEmpty()) {
+                roleRepository.save(Role.builder()
+                        .name("ROLE_GUEST")
+                        .description("Usuario invitado")
+                        .build());
+            }
+
+        };
+    }
+}
